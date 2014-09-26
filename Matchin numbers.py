c871394b3804d6362f65e4d95f0c0a9fa6f81530
34527 <34527@longroad.ac.uk>
2014-09-26 14:33:09 +0100

    Class work 26/09/2014

---
+++ Matchin numbers.py
@@ -0,0 +1,11 @@
+#Euan McElhoney
+#26/09/2014
+#Selection class exercise - Revision 1
+
+first_number = int(input("Please enter your first number: "))
+second_number = int(input("Please enter your second number: "))
+
+if first_number == second_number:
+    print("The numbers match")
+else:
+    print("The numbers do not match")
