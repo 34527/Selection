1d3732d174b543d32f99a6ea93d73277292406aa
34527 <34527@longroad.ac.uk>
2014-10-03 08:57:16 +0100

    Work from home 3/10/2014

--- Grade Boundaries.py
+++ Grade Boundaries.py
@@ -6,4 +6,15 @@ grade = int(input("Please enter the recieved grade: "))
 
 if 81 <= grade <= 100:
     print("Grade A")
-elif 71<= grade <= 80
+elif 71<= grade <= 80:
+    print("Grade B")
+elif 61<= grade <= 70:
+    print("Grade C")
+elif 51<= grade <= 60:
+    print("Grade D")
+elif 41 <= grade <= 50:
+    print("Grade E")
+elif 101 <= grade:
+    print("Invalid score")
+else:
+    print("Fail")
